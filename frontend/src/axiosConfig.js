c37185b41f9acb7f1ec3273302a952e2cf2fe768
Ngo Cong Huan <conghuan03012003@gmail.com>
2024-09-19 11:03:09 +0700

    v2

--- frontend/src/axiosConfig.js
+++ frontend/src/axiosConfig.js
@@ -1,4 +1,4 @@
 import axios from 'axios';
 
 axios.defaults.baseURL =
-  process.env.NODE_ENV !== 'production' ? 'http://localhost:5000' : '/';
+  process.env.NODE_ENV !== 'production' ? 'http://20.248.209.12:5000' : '/';
